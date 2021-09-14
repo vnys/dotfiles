5fde6a386abdc8b62a036d785e4afc6e378045df
Victor Nystad <vnys@equinor.com>
2021-09-14 06:02:11 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,9 @@
 export DEBIAN_FRONTEND=noninteractive
 
 sudo apt-get update
-sudo apt-get -y install --no-install-recommends fish
+sudo apt-get -y install --no-install-recommends fish wget unzip
+
+# Install Jetbrains Mono font
+wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
+sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
+sudo fc-cache -f -v
