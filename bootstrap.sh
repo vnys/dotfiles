43b30fcd124e4e5964f4a0a93b475c7803168327
Victor Nystad <vnys@equinor.com>
2021-09-14 06:43:08 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 sudo apt-get update
 sudo apt-get -y install --no-install-recommends fish wget unzip
 
-# Install Jetbrains Mono font
+# Install Jetbrains Mono font (no effect on ChromeOS)
 wget https://download.jetbrains.com/fonts/JetBrainsMono-2.001.zip
 sudo unzip JetBrainsMono-2.001.zip -d /usr/share/fonts
 sudo fc-cache -f -v
