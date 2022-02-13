3d1f80a3768ae7f3b8b07d5c5ff036f93837124e
Victor Nystad <vnys@equinor.com>
2022-02-13 14:06:05 +0100

    Add starship

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,8 @@ sudo apt-get -y install --no-install-recommends fish wget unzip neovim
 wget https://github.com/ogham/exa/releases/download/v0.10.1/exa-linux-x86_64-v0.10.1.zip
 sudo unzip exa-linux-x86_64-v0.10.1.zip -d $HOME/.local
 
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+
 cp -fr ./.config/fish $HOME/.config/
 cp -fr ./.themes $HOME/
 cp -f ./.gitconfig $HOME/
